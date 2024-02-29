156587bf88f4e9ddff5e4fa8ea4133c43203dd2f
shpetimsedolli <shpeti101@gmail.com>
2024-02-29 17:45:08 +0100

    added components with configurations

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,24 @@
 import React from "react";
+import styles from "./style";
+
+import {Billing , Business , Button , CardDeal , Clients , CTA , FeedbackCard , Footer , GetStarted , Hero , Navbar , Stats , Testimonials}
 
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}>Navbar</div>
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>Hero</div>
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Stats Business Billing CardDeal Testimonial Clients CTA Footer
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default App;
