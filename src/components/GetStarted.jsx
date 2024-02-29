54c60e0ff8b45e43c9b03154fdc331801345709e
shpetimsedolli <shpeti101@gmail.com>
2024-02-29 20:41:03 +0100

    added Stats component with style

--- src/components/GetStarted.jsx
+++ src/components/GetStarted.jsx
@@ -9,7 +9,7 @@ const GetStarted = () => {
         className={`${styles.flexCenter} flex-col bg-primary w-[100%] h-[100%] rounded-full `}
       >
         <div className={`${styles.flexStart} flex-row`}>
-          <p className="font-poppins font-medium text-[18px] leading-[23px]">
+          <p className="font-poppins font-medium text-[18px] mr-2 leading-[23px]">
             <span className="text-gradient">Get</span>
           </p>
           <img
