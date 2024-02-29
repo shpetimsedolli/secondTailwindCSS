8679f157403622586efe24fcc0b365e66df384a1
shpetimsedolli <shpeti101@gmail.com>
2024-02-29 18:45:48 +0100

    navbar Completed

--- src/App.jsx
+++ src/App.jsx
@@ -26,7 +26,9 @@ const App = () => {
         </div>
       </div>
       <div className={`bg-primary ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>Hero</div>
+        <div className={`${styles.boxWidth}`}>
+          <Hero />
+        </div>
       </div>
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,6 +28,25 @@ const Navbar = () => {
           className="w-[28px] h-[28px] object-contain"
           onClick={() => setToggle((prev) => !prev)}
         />
+        <div
+          className={`${
+            toggle ? "flex" : "hidden"
+          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}
+        >
+          <ul className="list-none flex flex-col  justify-end items-center flex-1 ">
+            {navLinks.map((nav, index) => (
+              <li
+                key={nav.id}
+                className={`font-poppins 
+                    font-normal cursor-pointer text-[16px] text-white mb-10 ${
+                      index === navLinks.length - 1 ? "mb-0" : "mb-10"
+                    }`}
+              >
+                <a href={`#${nav.id}`}>{nav.title}</a>
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </nav>
   );
