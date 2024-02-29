a5cd6fd495fa0d5fde767111efd2493cc9cf8c24
shpetimsedolli <shpeti101@gmail.com>
2024-02-29 20:03:45 +0100

    hero section and left content

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,9 +31,9 @@ const Navbar = () => {
         <div
           className={`${
             toggle ? "flex" : "hidden"
-          } p-6 bg-black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] rounded-xl sidebar `}
+          }  bg-black-gradient absolute top-20 right-0  h-screen   w-full  sidebar `}
         >
-          <ul className="list-none flex flex-col  justify-end items-center flex-1 ">
+          <ul className="list-none flex flex-col pt-4 items-center flex-1 ">
             {navLinks.map((nav, index) => (
               <li
                 key={nav.id}
