4383b185d9ab1445f4b115b1dbceb49da2fd4888
shpetimsedolli <shpeti101@gmail.com>
2024-02-29 18:33:03 +0100

    added close and menu button in responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { close, logo, menu } from "../assets";
 import { navLinks } from "../constants";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
   return (
     <nav className="w-full flex py-6 justify-between items-center navbar">
       <img src={logo} className="w-[124px] h=[32px]" alt="" />
@@ -20,6 +21,14 @@ const Navbar = () => {
           </li>
         ))}
       </ul>
+      <div className="sm:hidden flex flex-1 justify-end items-center">
+        <img
+          src={toggle ? close : menu}
+          alt="menu"
+          className="w-[28px] h-[28px] object-contain"
+          onClick={() => setToggle((prev) => !prev)}
+        />
+      </div>
     </nav>
   );
 };
