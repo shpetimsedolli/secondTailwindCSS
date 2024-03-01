cb49d50213bce0acdad45c7fa99fb99cc405b194
shpetimsedolli <shpeti101@gmail.com>
2024-03-01 12:26:58 +0100

    Clients section added and ClientsTitle with style

--- src/components/FeedbackCard.jsx
+++ src/components/FeedbackCard.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import { quotes } from "../assets";
 
-const FeedbackCard = () => {
-  return <div></div>;
+const FeedbackCard = ({ content, name, title, img }) => {
+  return (
+    <div className="flex justfiy-between flex-col px-10 py-12 rounded-[20px] max-w-[370px] md:mr-10 sm:mr-5 mr-0 my-5 feedback-card">
+      <img src={quotes} alt="" />
+    </div>
+  );
 };
 
 export default FeedbackCard;
